cf4d90f87019c869f417b5f0a805d983e43aa0ad
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-01 13:17:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(new_word)
+    w = @word
+    w = w.split("")
+    n = new_word.split("")
+    if w.length == n.length && w.sort == n.sort
+      TRUE
+    else
+      FALSE
+    end
+end
